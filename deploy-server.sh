724f5b9523a983c3f3ac1fe6d1ac1323d30e725b
Damian Lienhart <142167244+damian-lienhart@users.noreply.github.com>
2025-06-25 10:28:32 +0200

    Update deploy-server.sh

--- deploy-server.sh
+++ deploy-server.sh
@@ -190,8 +190,8 @@ fi
 
 # --- ENV Config ---
 configure_env_file() {
-  echo "Enter your domain (leave empty or type 'localhost' to use http://localhost): "
-  read APP_DOMAIN
+  read -p "Enter your domain (leave empty or type 'localhost' to use http://localhost): " APP_DOMAIN
+  APP_DOMAIN=$(echo "$APP_DOMAIN" | tr -d ' ') 
 
 
   while :; do
