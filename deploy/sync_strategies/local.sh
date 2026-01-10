37c510b08ece16cbe840f3c493bff390fbab14e6
Pascal Feller <fellp1@students.bfh.ch>
2026-01-10 17:01:16 +0100

    docs: readme

--- deploy/sync_strategies/local.sh
+++ deploy/sync_strategies/local.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+SYNC_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(cd "$SYNC_SCRIPT_DIR/.." && pwd)"
 
 source "$ROOT_DIR/lib/ui.sh"
 source "$ROOT_DIR/lib/util.sh"
